b050dde9410ffa13eda27712c4695e73b7eba6ca
alexandregjr <alexgalocha.jr@gmail.com>
2021-04-26 14:16:22 -0300

    adding python deps to installer

--- installer.sh
+++ installer.sh
@@ -2,9 +2,13 @@
 
 sudo apt update
 
+echo "Installing python and pip..."
+sudo apt install python3 python3-pip -y
+
 echo "Installing dependecies..."
-sudo apt install pandoc
-sudo apt install texlive-latex-extra
+sudo apt install pandoc -y
+sudo apt install texlive-latex-extra -y
+pip3 install pypandoc
 
 echo "Installing code2pdf..."
 sudo cp ./code2pdf /usr/bin/code2pdf
